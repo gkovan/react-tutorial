8f65dca406594b986fdee33eb6a46a7ab095c617
gkovan <gkovan@hotmail.com>
2020-09-16 23:50:38 -0400

    tutorial complete

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-tutorial
+This is the tutorial i am doing:
+https://welearncode.com/complete-beginners-guide-react-2020/
---
+++ color-switcher/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import './App.css';
+import ColorChangeButton from './ColorChangeButton'
+
+
+function App() {
+  const [color, setColor] = useState('')
+  return (
+    <div className={`react-root ${color}`}>
+      <div className='centered'>
+        <h1>Color Picker</h1>
+        {color}
+        <ColorChangeButton color='red' setColor={setColor}/>
+        <ColorChangeButton color='blue'setColor={setColor}/>
+        <ColorChangeButton color='yellow'setColor={setColor}/>
+        <button className='red' onClick={() => setColor('red')}>red</button>
+        <button className='blue' onClick={() => setColor('blue')}>blue</button>
+        <button className='yellow' onClick={() => setColor('yellow')}>yellow</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
